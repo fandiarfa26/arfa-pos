9559aac8744660d8d5fb6edb17ddfde5d3958cea
arfabuma <arfabuma.987@gmail.com>
2026-06-13 19:37:03 +0700

    fix(utils): hoist Intl.DateTimeFormat to module scope for reuse

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,9 +1,11 @@
-export function formatDateTime(isoString: string) {
-	return new Intl.DateTimeFormat('id-ID', {
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-		hour: '2-digit',
-		minute: '2-digit'
-	}).format(new Date(isoString));
+const formatter = new Intl.DateTimeFormat('id-ID', {
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+	hour: '2-digit',
+	minute: '2-digit'
+});
+
+export function formatDateTime(isoString: string): string {
+	return formatter.format(new Date(isoString));
 }
