b334d4fac42291858b373e7fa6e009968f7d6515
arfabuma <arfabuma.987@gmail.com>
2026-06-18 10:42:11 +0700

    feat: add PageHeader to products list page

--- src/routes/(app)/(main)/products/+page.svelte
+++ src/routes/(app)/(main)/products/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import PageHeader from '../../../../shared/components/page-header.svelte';
 	import ProductList from '$features/products/components/product-list.svelte';
 
 	const { data } = $props();
@@ -7,5 +8,7 @@
 <svelte:head>
 	<title>Products - ArfaPOS</title>
 </svelte:head>
+
+<PageHeader title="Produk" />
 
 <ProductList products={data.products} search={data.search} />
