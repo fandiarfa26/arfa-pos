11732f7626d7eb60be00be69953ad9d8f4fe69a3
arfabuma <arfabuma.987@gmail.com>
2026-06-18 10:59:27 +0700

    fix: remove page header in product page

--- src/routes/(app)/(main)/products/+page.svelte
+++ src/routes/(app)/(main)/products/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import PageHeader from '$shared/components/page-header.svelte';
 	import ProductList from '$features/products/components/product-list.svelte';
 
 	const { data } = $props();
@@ -8,7 +7,5 @@
 <svelte:head>
 	<title>Products - ArfaPOS</title>
 </svelte:head>
-
-<PageHeader title="Produk" />
 
 <ProductList products={data.products} search={data.search} />
